86833a84b0b283a5623c1c523a3991aa7900a940
Basant kumar Hota <basahota@users.noreply.github.com>
2017-07-02 23:28:33 +0530

    #Code updated

--- znod/tech/interview/question/RailwayTime.java
+++ znod/tech/interview/question/RailwayTime.java
@@ -1,6 +1,9 @@
 package com.znod.tech.interview.question;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RailwayTime {
@@ -28,16 +31,46 @@ public class RailwayTime {
 		return begin + c + end;
 	}
 
-	public static void main(String[] args) {
-
-		String s2 = "1234";
-		Set<String> set = permutationFinder(s2);
+	public static List<Integer> getHighestTime(Set<String> set)
+			throws IllegalAccessException {
+		List<Integer> times = new ArrayList<Integer>();
 		for (String s : set) {
 			if (!s.startsWith("4")) {
 				if (!s.startsWith("3")) {
-					System.out.println(s);
+					times.add(Integer.valueOf(s));
 				}
+			} else {
+				throw new IllegalAccessException("Invalid Input");
 			}
 		}
+		Collections.sort(times);
+		List<String> ts = formatTime(times);
+		System.out.println("List of possible Times :" + ts);
+		return times;
+	}
+
+	private static List<String> formatTime(List<Integer> times) {
+		List<String> ts = new ArrayList<String>();
+		String s = "";
+		for (int i : times) {
+			s = String.valueOf(i);
+			ts.add(formater(s));
+		}
+		return ts;
+	}
+
+	private static String formater(String s) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(s.substring(0, 2)).append(":")
+				.append(s.substring(2, s.length()));
+		return sb.toString();
+	}
+
+	public static void main(String[] args) throws IllegalAccessException {
+		String s2 = "1234";
+		Set<String> set = permutationFinder(s2);
+		List<Integer> times = getHighestTime(set);
+		System.out.println("HighestTime :"
+				+ formater(String.valueOf(times.get(times.size() - 1))));
 	}
 }
